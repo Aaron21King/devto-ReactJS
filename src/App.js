007f555ce90de12e1ff0907c4b97b53a9342b78f
Daniel Aguilar <ing.aguilardan@gmail.com>
2022-07-01 14:39:13 -0500

    merge danny branch with daniel

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
-import Navigation from "./components/Nabvar";
-import LeftSidebar from "./components/LeftSidebar";
-import Content from "./components/Content";
-import RightSidebar from "./components/RightSidebar";
-import { useState } from "react";
+// import { useState } from "react";
 import "../src/App.scss";
 
+import { Routes, Route } from "react-router-dom";
+
+//containers
+import HomeContainer from "./containers/home/home";
+import LoginContainer from "./containers/Login/Loginpadre";
 function App() {
-  const [mobileMenu, setmobileMenu] = useState(false);
-
-  const toggle = () => {
-    setmobileMenu(!mobileMenu);
-  };
-
   return (
-    <>
-      <Navigation openMenu={toggle} />
-
-      <main className="main-container">
-        <LeftSidebar burgerMenu={mobileMenu} closeMenu={toggle} />
-        <Content />
-        <RightSidebar />
-      </main>
-    </>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<HomeContainer />} />
+        <Route path="login" element={<LoginContainer />} />
+      </Routes>
+    </div>
   );
 }
 
